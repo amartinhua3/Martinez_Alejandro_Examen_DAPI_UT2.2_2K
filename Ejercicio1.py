21b14bd8c832011a9b12653eb5260c7564ba299e
amartinhua3 <amartinhua3@educacion.navarra.es>
2023-12-21 16:30:31 +0100

    EALEJERCICIO1

---
+++ Ejercicio1.py
@@ -0,0 +1,8 @@
+def LeerDocumento():
+    '''Esta función lee la información deun fichero y la guarda en una variable de tipo lista
+        Parámetros: 
+            Entrada:'''
+    file = open('LibroCuentas.txt', 'r')
+    linea = open.readlines(file)
+    print(linea)
+
